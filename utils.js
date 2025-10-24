394f5421c631a8ad6651eb0da978d4658b989da3
Martin Sidaway <2389068+martinjos@users.noreply.github.com>
2025-10-25 00:28:29 +0100

    Create utils.js: proto(s) and dir

---
+++ utils.js
@@ -0,0 +1,29 @@
+proto = Object.getPrototypeOf;
+
+// Get the whole prototype chain
+function protos(obj) {
+  let ps = [];
+  obj = proto(obj);
+  while (obj !== null) {
+    ps.push(obj);
+    obj = proto(obj);
+  }
+  return ps;
+}
+
+function dir(obj) {
+  let d = [];
+  let ps = protos(obj);
+  ps.splice(0, 0, obj);
+  let ns = [];
+  let nh = {};
+  for (const p of ps) {
+    for (const n of Object.getOwnPropertyNames(p)) {
+      if (! (n in nh)) {
+        nh[n] = true;
+        ns.push(n);
+      }
+    }
+  }
+  return ns;
+}
